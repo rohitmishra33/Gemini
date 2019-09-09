4a7fafb8c5757c55cc501ed19fa6041cb76fe534
Rohit Mishra <callrohit33@gmail.com>
2019-09-09 23:43:15 +0530

    Adding 0 to the add score dropdown

--- game-dashboard-frontend/src/app/add-score/add-score.component.ts
+++ game-dashboard-frontend/src/app/add-score/add-score.component.ts
@@ -10,7 +10,7 @@ export class AddScoreComponent implements OnInit {
 
 	players: string[];
 	game = [];
-	possibleScores: number[] = [-5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+	possibleScores: number[] = [-5, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
 	constructor(private scoreService: ScoreService) {
 		this.scoreService.getPlayers().subscribe(data => {
